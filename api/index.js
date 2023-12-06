782d1a797813116b84e057abd0f40462aa9706d8
ismail muhammad adam <ismailgrapix@gmail.com>
2023-12-06 23:00:26 +0100

    connect to mongo database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+
+dotenv.config()
+
+
+mongoose.connect(process.env.MONGO).then( () =>{
+    console.log("connect to mongodb!")
+}).catch((err) => { 
+    console.log(err)
+}) 
 
 const app = express()
 
 app.listen(3000, () =>{
-    console.log('server i    s running on port 3000')
+    console.log('server is running on port 3000')
 })
