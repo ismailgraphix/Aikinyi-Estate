39547479ad1c9b97a5561ab64f4e42f219ee8916
ismail muhammad adam <ismailgrapix@gmail.com>
2023-12-14 17:46:31 +0100

    updating the header for profile and make the profile private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import Profile from "./pages/Profile"
-import Header from "./components/Header"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import PrivateRoutes from "./components/privateRoutes"; 
+
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
-    <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/Sign-In" element={<SignIn/>}/>
-    <Route path="/Sign-Up" element={<SignUp/>}/>
-    <Route path="/About" element={<About/>}/>
-    <Route path="/Profile" element={<Profile/>}/>
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Sign-In" element={<SignIn />} />
+        <Route path="/Sign-Up" element={<SignUp />} />
+        <Route path="/About" element={<About />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path="/Profile" element={<Profile />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,8 +2,41 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1>hello sjhvkk</h1>
+    <div className="bg-gray-100 min-h-screen">
+      
+
+      {/* Main Content */}
+      <main className="container mx-auto px-4 py-8">
+        {/* Featured Properties */}
+        <section className="mb-8">
+          <h2 className="text-2xl font-semibold mb-4">Featured Properties</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            {/* Property cards */}
+            <div className="bg-white shadow-md p-4 rounded-md">
+              <h3 className="text-xl font-semibold mb-2">Property Title</h3>
+              <p className="text-gray-600 mb-4">
+                Description of the property...
+              </p>
+              {/* Other details like images, price, etc. */}
+            </div>
+            {/* Repeat property cards for featured listings */}
+          </div>
+        </section>
+
+        {/* About Us */}
+        <section>
+          <h2 className="text-2xl font-semibold mb-4">About Us</h2>
+          <p className="text-gray-600">
+            Brief description about your estate agency or website...
+          </p>
+          {/* Other sections like services, testimonials, etc. */}
+        </section>
+      </main>
+
+      {/* Footer */}
+      <footer className="bg-gray-800 text-white text-center py-4">
+        <p>&copy; Your Estate {new Date().getFullYear()}</p>
+      </footer>
     </div>
   )
 }
